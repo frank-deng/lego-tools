1382bc80c480148e274821387d738d21d96783d5
frank-deng <13382084679@163.com>
2021-07-28 16:00:36 +0800

    detect-hubs.js add battery level display

--- detect-hubs.js
+++ detect-hubs.js
@@ -2,7 +2,7 @@ const PoweredUP=require('node-poweredup');
 const poweredUp=new PoweredUP.PoweredUP();
 
 poweredUp.on('discover',(hub)=>{
-  console.log(`Hub Discovered, ID: ${hub.uuid}    Name: ${hub.name}`);
+  console.log(`Hub Discovered, ID: ${hub.uuid}    Name: ${hub.name}    Battery: ${hub.batteryLevel}%`);
 });
 console.log('Start scanning...');
 poweredUp.scan();
